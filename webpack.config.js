8baf7ecad9c9f0e66eea96d77f332a9e2f9cf37b
Qvazar <sebjorg@gmail.com>
2016-02-25 21:39:44 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './build',
+    filename: 'main.js'       
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    // you can now require('file') instead of require('file.coffee')
+    extensions: ['', '.js', '.json', '.coffee'] 
+  }
+};
+
